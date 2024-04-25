0f20534a57b8c5a13689434f3398cc996cf7a6ec
oytel <143611861+oytel@users.noreply.github.com>
2024-04-25 21:14:00 +0200

    div

--- src/main/java/oslomet/webprog/data1700oblig3/KinoKjopController.java
+++ src/main/java/oslomet/webprog/data1700oblig3/KinoKjopController.java
@@ -1,2 +1,14 @@
-package oslomet.webprog.data1700oblig3;public class KinoKjopController {
+package oslomet.webprog.data1700oblig3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KinoKjopController {
+    @GetMapping("/")
+    public String hei(String navn) {
+        int antallBokstaver = navn.length();
+
+        return "Hello World " + navn + "! Navnet ditt er " + antallBokstaver + " bokstaver langt!";
+    }
 }
