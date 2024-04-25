7faa652392245635b87d735a6ca0a033a3c7482e
oytel <143611861+oytel@users.noreply.github.com>
2024-04-25 21:05:51 +0200

    Initial commit

---
+++ src/main/java/oslomet/webprog/data1700oblig3/KinoKjopController.java
@@ -0,0 +1,2 @@
+package oslomet.webprog.data1700oblig3;public class KinoKjopController {
+}
---
+++ src/test/java/oslomet/webprog/data1700oblig3/Data1700Oblig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.webprog.data1700oblig3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
